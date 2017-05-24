5fa3d3be6d3a8f908f54ed9d330744c1db3ccb55
Gonzalo Lucero <gonzigonz@users.noreply.github.com>
2017-05-24 13:28:59 +1000

    Buidling Console Apps

---
+++ XPlat/LinkChecker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HtmlAgilityPack;
+
+namespace XPlat
+{
+    public class LinkChecker
+    {
+        public static IEnumerable<string> GetLinks(string page)
+        {
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(page);
+            var links = htmlDocument.DocumentNode.SelectNodes("//a[@href]")
+                                .Select(n => n.GetAttributeValue("href", string.Empty))
+                                .Where(l => !String.IsNullOrEmpty(l))
+                                .Where(l => l.StartsWith("http", StringComparison.CurrentCultureIgnoreCase));
+
+            return links;
+        }
+    }
+}
